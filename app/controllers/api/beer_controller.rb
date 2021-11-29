9f0b0c681e26f143c43e0e399d625e17ecccbdef
Austin <austinchoi22@gmail.com>
2021-11-29 15:35:20 -0700

    Seeds and Controller

--- app/controllers/api/beer_controller.rb
+++ app/controllers/api/beer_controller.rb
@@ -1,2 +1,51 @@
 class Api::BeerController < ApplicationController
+  before_action :set_beer, only: [:show, :destroy, :update]
+
+# get /api/beers
+  def index
+    render json: Beer.all
+  end
+
+# get /api/beers:id
+  def show
+    render json: @beer
+  end
+
+# post /api/beers
+  def create
+    beer = Beer.new(beer_params)
+    if beer.save
+      render json: beer
+    else
+      render json: { errors: beer.errors }, status: :unprocessable_entity
+    end
+  end
+
+# put or patch /api/beers/:id
+  def update
+    beer = Beer.find(params[:id])
+    if beer.update(beer_params)
+      render json: beer
+    else
+      render json: { errors: beer.errors }, status: :unprocessable_entity
+    end
+  end
+
+# delete /api/beers/:id
+  def destroy 
+    beer = Beer.find(params[:id]).destroy
+    render json: beer
+  end
+
+  private
+
+  def beer_params
+    params.require(:beer).permit(:name, :abv, :style)
+  end
+
+  def set_beer
+    @beer = @Beer.find(params[:id])
+  end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Beer.create(name: "Relentless, Green Flash Saturhaze Hazy", abv:4.2, style: "Session Hazy IPA" )
+Beer.create(name: "Mother Earth, Cali Creamin", abv:5, style:"Vanilla Cream Ale" )
+Beer.create(name: "Belching Beaver, Peanut Butter Stout", abv:5.4, style:"Stout")
+Beer.create(name: "Green Flash, West Coast IPA", abv:7, style:"IPA")
+Beer.create(name: "Relentless, The Cynic Golden Blackberry, Rasberry Sour", abv:6.1, style:"Sour")
+puts "seeded beer size: #{Beer.all.size}"
