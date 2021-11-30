87986ce8599c3751dcc7b3c5e9c4d9ebfd41655e
Austin <austinchoi22@gmail.com>
2021-11-30 15:42:18 -0700

    assigned routes to app and about

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Beers from "./components/Beers";
-import {Route,Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import About from "./components/About"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Beers />} />
+        <Route path="/beers/:id" element={<About />} />
       </Routes>
     </div>
   );
