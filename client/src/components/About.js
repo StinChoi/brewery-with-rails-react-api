ccb95937e9cc605c0dcf03ea04b981a53d65f92b
Austin <austinchoi22@gmail.com>
2021-11-30 18:00:14 -0700

    about and beer form

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,23 +5,27 @@ import axios from "axios"
 import BeerForm from "./BeerForm"
 
 const About = () => {
-  const [beer, setBeer] = useState({});
+  const [beer, setBeer] = useState("");
   const params = useParams();
   const getBeer = async () => {
     let result = await axios.get(`/api/beers/${params.id}`)
     setBeer(result.data);
+    console.log(result.data);
   }
   useEffect(() => {
     getBeer();
   }, []);
-  // update beer function goes here
+  const updateBeer = (beer) => {
+    setBeer(beer)
+  }
+  console.log(beer.name);
   return (
     <div>
       <h1>About Beer</h1>
-      <p></p>
+      <p>{beer.name}</p>
       <h3>Edit Beer</h3>
-      <BeerForm />
-
+      {/* <BeerForm id={beer.id} name={beer.name} abv={beer.abv} updateBeer={updateBeer} /> */}
+      <BeerForm id={beer.id} name={"y"} abv={beer.abv} updateBeer={updateBeer} />
     </div>
   )
 };
--- client/src/components/BeerForm.js
+++ client/src/components/BeerForm.js
@@ -3,22 +3,30 @@ import React, { useState } from "react";
 import BorderedDiv from "./BorderedDiv";
 
 const BeerForm = (props) => {
-  const {addBeer} = props;
-  const [name, setName] = useState("");
-  const [abv, setAbv] = useState("");
+  const { addBeer, id, name: oldName, abv: oldAbv, updateBeer } = props;
+  const [name, setName] = useState(oldName ? oldName : "x");
+  const [abv, setAbv] = useState(oldAbv ? oldAbv : "");
   const handleSubmit = async (e) => {
     e.preventDefault();
     // console.log("Submit Clicked");
     // console.log({ name, abv });
     const newBeer = {
-      name:name,
-      abv:abv,
+      name: name,
+      abv: abv,
     };
-    let result = await axios.post("/api/beers", newBeer)
-    addBeer(result.data);
-    setName("");
-    setAbv("");
+    if (id) {
+      let result = await axios.put(`/api/beers/${id}`, newBeer)
+      updateBeer(result.data);
+    } else {
+      let result = await axios.post("/api/beers", newBeer)
+      addBeer(result.data);
+      // setName("");
+      // setAbv("");
+
+    };
+
   };
+  console.log(name);
   return (
     <BorderedDiv color="black">
       <form onSubmit={handleSubmit}>
@@ -33,7 +41,6 @@ const BeerForm = (props) => {
           type="number"
           value={abv}
           onChange={(e) => setAbv(e.target.value)}
-
         />
         <button type="enter">Add</button>
       </form>
